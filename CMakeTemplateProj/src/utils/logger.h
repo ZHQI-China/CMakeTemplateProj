b7ff126d9ce3e5986609c8478c9ae19c6db16cfd
ZHQI <2104229519@qq.com>
2026-01-20 19:13:16 +0800

    初始化

---
+++ CMakeTemplateProj/src/utils/logger.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <spdlog/spdlog.h>
+
+#define LogLevel(x) spdlog::set_level(spdlog::level::x)
+
+#define LogTrace(...)	spdlog::trace(__VA_ARGS__)
+#define LogInfo(...)	spdlog::info(__VA_ARGS__)
+#define LogDebug(...)	spdlog::debug(__VA_ARGS__)
+#define LogWarn(...)	spdlog::warn(__VA_ARGS__)
+#define LogError(...)	spdlog::error(__VA_ARGS__)
+
+
+//截取文件名称：windows: '\\'  Linux: '/'
+#define __FILENAME__ (strrchr(__FILE__, '\\') ? (strrchr(__FILE__, '\\') + 1):__FILE__)
+
+// 辅助宏：处理带位置的日志
+#define LOG_WITH_LOCATION(level, ...) \
+    spdlog::level("-[{}:{} | {}]- {}",__FILENAME__, __LINE__, __func__,fmt::format(__VA_ARGS__))
+
+// 定义带位置的日志宏// L: location
+#define LogTraceL(...) LOG_WITH_LOCATION(trace, __VA_ARGS__)
+#define LogInfoL(...)  LOG_WITH_LOCATION(info, __VA_ARGS__)
+#define LogDebugL(...) LOG_WITH_LOCATION(debug, __VA_ARGS__)
+#define LogWarnL(...)  LOG_WITH_LOCATION(warn, __VA_ARGS__)
+#define LogErrorL(...) LOG_WITH_LOCATION(error, __VA_ARGS__)
+
+// todo: 写入日志文件
---
+++ CMakeTemplateProj/test/test00-env.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include "utils/logger.h"
+
+void test_logger() {
+
+	spdlog::set_level(spdlog::level::trace);
+
+	LogTrace("{}", "trace text");
+	LogInfo("数字：{}  {}  {}  {}", true, 1, 1.2f, 2.3);
+
+	int i = 1;
+	float j = 1.2f;
+	double k = 1.23;
+
+	LogDebug("精度：--{}--{:.2f}--{:.8f}--", i, j, k);
+	LogWarn("字符串：{} {}", "c_str: 中文", std::string("std::string(中文)"));
+	LogError("换序：{0} {2} {1}", "step1", "step2", "step3");
+
+	LogInfo("\n");
+	LogInfo("-------------Loction test-------------");
+	LogDebugL("L精度：--{}--{:.2f}--{:.8f}--", i, j, k);
+	LogWarnL("L字符串：{} {}", "c_str: 中文", std::string("std::string(中文)"));
+	LogErrorL("L换序：{0} {2} {1}", "step1", "step2", "step3");
+
+}
+
+
+
+int main(int argc, char* argv[]){
+    std::cout << "Hello World!" << std::endl;
+
+    test_logger();
+
+    return 0;
+}
